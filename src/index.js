b43d68dc312b23f8ecafa66abc7f1a9e0f3909b8
chamroro <chamsae@handong.ac.kr>
2024-08-06 20:19:14 +0900

    .

--- src/index.js
+++ src/index.js
@@ -24,9 +24,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export { app };
